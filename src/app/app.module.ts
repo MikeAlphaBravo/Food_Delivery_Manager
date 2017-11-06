9df681c3d1450fd8d65ce00f0952e43a6bdf4c77
Michael Brooks <mikeyabrooks@gmail.com>
2017-11-06 09:46:51 -0800

    add a base file project folder and structure for JavaScript week 5 group project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientDetailComponent } from './client-detail/client-detail.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MapComponent } from './map/map.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClientListComponent,
+    ClientDetailComponent,
+    CalendarComponent,
+    MapComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule
