ce363aa8c975d81b8e36b56c25511729298b7468
Guest User <Guest@Epicodus-6E.local>
2017-11-06 13:51:16 -0800

    start making stripe call

--- src/app/payment.service.ts
+++ src/app/payment.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { masterStripeConfig } from './api-keys'
 
 @Injectable()
 export class PaymentService {
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { masterStripeConfig } from '../api-keys'
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent implements OnInit {
+export class PaymentComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  openCheckout() {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: masterStripeConfig,
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+      }
+    });
 
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: 2000
+    });
+  }
 }
