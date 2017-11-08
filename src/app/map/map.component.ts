fb461c0d1c3cff2a3c96b80994711d5e58c416e7
Michael Brooks <mikeyabrooks@gmail.com>
2017-11-08 14:57:48 -0800

    testing a subscribe function

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [ MapService ]
 })
 export class MapComponent implements OnInit {
-  title: string = 'Portland';
-  lat: number = 45.5231;
-  lng: number = -122.6765;
 
-  constructor() { }
-
+  constructor(private mapAddress: MapService) { }
+  getAddressBySearch(address: string) {
+    let test = this.mapAddress.getAddressBySearch(address);
+    debugger;
+  }
   ngOnInit() {
+
+  }
+
+  testMethod() {
+    debugger
   }
 
 }
