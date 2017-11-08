b29d669ddb42ba30add0f6e4adb5b6144eb5680e
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-11-08 09:30:02 -0800

    add meal

---
+++ src/app/meal.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Meal } from './meal.model';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+@Injectable()
+export class MealService {
+  meals: Observable<any[]>;
+
+  constructor(private database: AngularFirestore) {
+   this.meals = database.collection('meals').snapshotChanges()
+     .map(actions => {
+       return actions.map(a => {
+         const data = a.payload.doc.data() as Meal;
+         const id = a.payload.doc.id;
+         return{ id, data };
+       });
+     });
+  }
+
+  getMeals() {
+    return this.meals;
+  }
+
+  getMealById(mealId: string) {
+    return this.database.doc('meals/' + mealId).valueChanges();
+  }
+  createMeal(meal) {
+    return this.database.collection('meals').add(meal);
+  }
+}
