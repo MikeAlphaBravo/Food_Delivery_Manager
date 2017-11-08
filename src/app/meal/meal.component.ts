b1b748e26e987ff68497b9082bc1e957f07bc5a7
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-11-08 11:50:28 -0800

    fix meals

--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -32,15 +32,16 @@ export class MealComponent implements OnInit {
 
 
   submitForm(meal: string, date: string, clients: string) {
-    const clientList = clients.split(',');
-    const clientListAll = [];
+    // debugger;
+    let clientList = [];
+    if (clients !== "") {
+      clientList = clients.split(',');
+    } else {
     (this.clientsToDisplay).forEach(function(client) {
       if (client.data.opt === 'true') {
-        clientListAll.push(client.data.name);
+        clientList.push(client.data.name);
       }
     });
-    if (clientList === '[""]') {
-      clientList = clientListAll;
     }
     const newMeal: Object = ({meal: meal, date: date, clients: clientList});
     this.mealService.createMeal(newMeal);
