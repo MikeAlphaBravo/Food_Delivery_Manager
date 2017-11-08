5df809a3d9a6b51de8354e703b15581254880022
Michael Brooks <mikeyabrooks@gmail.com>
2017-11-08 10:47:16 -0800

    push to pull

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,13 +13,6 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // function initMap() {
-    //     var uluru = {lat: -25.363, lng: 131.044};
-    //     var map = new google.maps.Map(document.getElementById('map'), {
-    //       zoom: 4,
-    //       center: uluru
-    //     });
-    //   }
   }
 
 }
